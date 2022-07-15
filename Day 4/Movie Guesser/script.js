b05e427c6a730b416d4be7bce87282622edbc9eb
Tristan Chu <tristan8chu@gmail.com>
2022-07-15 00:03:04 -0700

    event listeners not working for no reason

---
+++ Day 4/Movie Guesser/script.js
@@ -0,0 +1,45 @@
+const movies = [
+    {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+    {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+    {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+    {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+    {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+    {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+    {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+    {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+    {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+    {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+    {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+   ];
+
+const guessInput = document.getElementById("guess");
+const submitInput = document.getElementById("submit");
+const hintInput = document.getElementById("hint");
+const cardBody = document.getElementsByClassName("card-body");
+
+const index = Math.floor(Math.random() * 11);
+const explanation = document.getElementById("explanation");
+explanation.innerHTML = movies[index].explanation;
+
+const answer = movies[index].title;
+const answerHint = movies[index].hint;
+
+submitInput.addEventListener('click', (event) => {
+    const submission = submitInput.value;
+    const subDiv = document.createElement('div');
+    if (submission === answer) {
+        subDiv.innerHTML = "You got it!";
+        subDiv.classList.add('alert-success');
+    } else {
+        subDiv.innerHTML = "Try again!";
+        subDiv.classList.add('alert-warning');
+    }
+    cardBody.append(subDiv);
+})
+
+hintInput.addEventListener('click', (event) => {
+    const hintDiv = document.createElement('div');
+    hintDiv.classList.add('display-3');
+    hintDiv.innerHTML = answerHint;
+    cardBody.append(hintDiv);
+})
